2ee55025e5383c496825c8702fab2b166c4af57b
Gergely Papp <gregpappdev@gmail.com>
2023-08-16 21:32:59 +0200

    add DbContext

---
+++ StepUpApi/StepUpApi/Data/ApiDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StepUpApi.Models;
+
+namespace StepUpApi.Data;
+
+public class ApiDbContext : DbContext
+{
+    public DbSet<Appointment> Appointments { get; set; }
+    public DbSet<AppointmentLog> AppointmentLos { get; set; }
+    public DbSet<ContactDetails> ContactDetails { get; set; }
+    public DbSet<ContactPerson> ContactPersons { get; set; }
+    public DbSet<Customer> Customers { get; set; }
+    public DbSet<EmployeeType> EmployeeTypes { get; set; }
+    public DbSet<ExaminationType> ExaminationTypes { get; set; }
+    public DbSet<Location> Locations { get; set; }
+    public DbSet<Owner> Owners { get; set; }
+    public DbSet<PeriodicInvoice> PeriodicInvoices { get; set; }
+    public DbSet<Role> Roles { get; set; }
+    public DbSet<Service> Services { get; set; }
+    public DbSet<User> Users { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(@"Server=localhost\SQLEXPRESS;Database=StepUpDb;");
+    }
+}
