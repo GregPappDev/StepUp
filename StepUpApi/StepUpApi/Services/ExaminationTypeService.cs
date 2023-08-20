1400ac3d48abefa140f55b6b73d79539ef1c7c2a
Gergely Papp <gregpappdev@gmail.com>
2023-08-20 17:22:28 +0200

    implement IExaminationTypeService interface without update method

---
+++ StepUpApi/StepUpApi/Services/ExaminationTypeService.cs
@@ -0,0 +1,48 @@
+﻿using StepUpApi.Data;
+using StepUpApi.Models;
+using StepUpApi.Services.Interfaces;
+
+namespace StepUpApi.Services
+{
+    public class ExaminationTypeService : IExaminationTypeService
+    {
+        private readonly ApiDbContext _context;
+        private IExaminationTypeService _service;
+
+        public ExaminationTypeService(ApiDbContext context, IExaminationTypeService service)
+        {
+            _context = context;
+            _service = service;
+        }
+
+        public IEnumerable<ExaminationType> GetAll()
+        {
+            return _context.ExaminationTypes;
+        }
+
+        public ExaminationType? GetById(Guid id)
+        {
+            return _context.ExaminationTypes.FirstOrDefault( e => e.Id == id);
+        }
+
+        public void Create(ExaminationType item)
+        {
+            if (_context.ExaminationTypes.Any(type => type.Type == item.Type)) return;
+            _context.ExaminationTypes.Add(item);
+            _context.SaveChanges();
+        }
+
+        public ExaminationType? Update(ExaminationType updatedData)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Guid id)
+        {
+            ExaminationType? examinationType = GetById(id);
+            if (examinationType == null) return;
+            _context.ExaminationTypes.Remove(examinationType);
+            _context.SaveChanges();
+        }
+    }
+}
---
+++ StepUpApi/StepUpApi/Services/Interfaces/IExaminationTypeService.cs
@@ -0,0 +1,14 @@
+﻿using StepUpApi.Models;
+
+namespace StepUpApi.Services.Interfaces
+{
+    public interface IExaminationTypeService
+    {
+        IEnumerable<ExaminationType> GetAll();
+        ExaminationType? GetById(Guid id);
+        void Create(ExaminationType item);
+        ExaminationType? Update(ExaminationType updatedData);
+        void Delete(Guid id);
+        
+    }
+}
