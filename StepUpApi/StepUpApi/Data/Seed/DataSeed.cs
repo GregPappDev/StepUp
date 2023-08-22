066dda7c4ab231b269f9f13f979418bb9d7893e1
Gergely Papp <gregpappdev@gmail.com>
2023-08-22 18:30:19 +0200

    add users to seed data

--- StepUpApi/StepUpApi/Data/Seed/DataSeed.cs
+++ StepUpApi/StepUpApi/Data/Seed/DataSeed.cs
@@ -1,4 +1,5 @@
 ﻿using StepUpApi.Models;
+using System.Security.Cryptography;
 
 namespace StepUpApi.Data.Seed
 {
@@ -14,6 +15,7 @@ namespace StepUpApi.Data.Seed
         public void CreateAll()
         {
             CreateExaminationTypes();
+            CreateUsers();
         }
 
         private void CreateExaminationTypes()
@@ -29,5 +31,38 @@ namespace StepUpApi.Data.Seed
             _context.ExaminationTypes.AddRange(types);
             _context.SaveChanges();
         }
+
+        private void CreateUsers()
+        {
+            List<string> users = new List<string>()
+            {
+                "Susan Smith",
+                "Michael Taylor"
+            };
+
+            foreach (var user in users)
+            {
+                var newUser = new User();
+                newUser.Name = user;
+                newUser.UserName = String.Concat(user.Where(c => !Char.IsWhiteSpace(c))).ToLower();
+                string password = Random.Shared.Next(100000, 999999).ToString();
+                CreatePasswordHash(password, out byte[] passwordHash, out byte[] passwordSalt);
+                newUser.PasswordHash = passwordHash; 
+                newUser.PasswordSalt = passwordSalt;
+                _context.Users.Add(newUser);
+                _context.SaveChanges(true);
+            }
+        }
+
+        private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            using (var hmac = new HMACSHA512())
+            {
+                passwordSalt = hmac.Key;
+                passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+            }
+        }
+
+
     }
 }
