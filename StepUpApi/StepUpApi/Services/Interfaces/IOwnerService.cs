8333c9688b2dbb7424fbfd998a18b04172d0bace
Gergely Papp <gregpappdev@gmail.com>
2023-09-04 14:52:10 +0200

    add service layer to Owner model

---
+++ StepUpApi/StepUpApi/Services/Interfaces/IOwnerService.cs
@@ -0,0 +1,14 @@
+﻿using StepUpApi.DTOs.Owner;
+using StepUpApi.Models;
+
+namespace StepUpApi.Services.Interfaces;
+
+public interface IOwnerService
+{
+    Task<ServiceResponse<IEnumerable<Owner>>> GetAll();
+    Task<ServiceResponse<IEnumerable<Owner>>> GetNotDeleted();
+    Task<ServiceResponse<Owner>> GetById(Guid id);
+    Task<ServiceResponse<Owner>> Create(UpdateOwnerDto dto);
+    Task<ServiceResponse<Owner>> Update(Guid id, UpdateOwnerDto updatedData);
+    Task<ServiceResponse<Owner>> Delete(Guid id);
+}
