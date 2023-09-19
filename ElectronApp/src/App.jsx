feb97cb6d2cb812fe487980f468179efe65d93ec
GregPappDev <gregpappdev@gmail.com>
2023-09-19 10:13:19 +0200

    add icon and change app title

--- ElectronApp/src/App.jsx
+++ ElectronApp/src/App.jsx
@@ -1,14 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/electron-vite.animate.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/electron-vite.animate.svg";
+//import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
+      <div class="container">
         <a href="https://electron-vite.github.io" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -28,8 +28,9 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <button class="btn btn-primary">Button</button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
